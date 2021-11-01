d153d8bd3e497bc0381c124319f9a1b82bacbba5
Luke Hufnagle <leh17068@gmail.com>
2021-11-01 14:40:09 -0400

    added validations

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -7,12 +7,18 @@ const UserForm = () => {
     let [email, setEmail] = useState("")
     let [password, setPassword] = useState("")
     let [confPass, setConfPass] = useState("")
+
     return(
         <>
             <form>
                 <div className="form-group">
                     <label htmlFor="">First Name: </label>
                     <input onChange= {(e)=>(setFirstName(e.target.value))} type="text" name="firstName" id="" className="form-control"/>
+                    {
+                        firstName.length <=2 && firstName.length > 0 ?
+                            <p className="text-danger">First name must be at least 2 characters!</p>
+                            : ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Last Name: </label>
@@ -21,14 +27,29 @@ const UserForm = () => {
                 <div className="form-group">
                     <label htmlFor="">Email: </label>
                     <input onChange= {(e)=>(setEmail(e.target.value))} type="text" name="email" id="" className="form-control"/>
+                    {
+                        email.length <=5 && email.length > 0 ?
+                            <p className="text-danger">Email must be at least 5 characters!</p>
+                            : ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Password: </label>
                     <input onChange= {(e)=>(setPassword(e.target.value))} type="text" name="password" id="" className="form-control"/>
+                    {
+                        password !== confPass && password.length <= 8?
+                            <p className="text-danger">Passwords must match and be at least 8 characters!</p>
+                            : ""
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Confirm Password: </label>
                     <input onChange= {(e)=>(setConfPass(e.target.value))} type="text" name="confPass" id="" className="form-control"/>
+                    {
+                        confPass !== password && confPass.length <= 8?
+                            <p className="text-danger">Passwords must match and be at least 8 characters!</p>
+                            : ""
+                    }
                 </div>
                 <input type="submit" value="Create User" className="btn btn-success mt-2" />
             </form>
